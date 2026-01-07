52057a98623f6eb8696b1d46be0326671debffb5
Lance Lin <lancerabbit@gmail.com>
2026-01-07 15:50:12 +0800

    feat(plugins): add typos-lsp for typo notification

---
+++ lazyvim/lua/plugins/typos-lsp.lua
@@ -0,0 +1,61 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        typos_lsp = {
+          filetypes = {
+            "lua",
+            "ruby",
+            "python",
+            "javascript",
+            "typescript",
+            "html",
+            "css",
+            "json",
+            "yaml",
+            "markdown",
+            "gitcommit",
+          },
+        },
+      },
+    },
+  },
+}
+
+-- return {
+--   {
+--     "neovim/nvim-lspconfig",
+--     opts = {
+--       vim.lsp.enable("cspell_ls"),
+--       vim.lsp.config("cspell_ls", {
+--         cmd = { "cspell-lsp", "--stdio" },
+--         filetypes = {
+--           "lua",
+--           "ruby",
+--           "python",
+--           "javascript",
+--           "typescript",
+--           "html",
+--           "css",
+--           "json",
+--           "yaml",
+--           "markdown",
+--           "gitcommit",
+--         },
+--         init_options = {
+--           -- cspell 特定設定
+--           customDictionaries = {
+--             custom = {
+--               name = "custom",
+--               path = vim.fn.expand("~/.dotfiles/my_dictionary/custom_dict.txt"),
+--               addWords = true,
+--             },
+--           },
+--         },
+--
+--         root_markers = { ".git" },
+--       }),
+--     },
+--   },
+-- }
